b2c68d3afdf5be12e0c6b29e3051a466e2cf6bd1
DonRichkenzo <66156722+DonRichkenzo@users.noreply.github.com>
2020-06-01 15:33:43 +0300

    Create main.java
    Вместо
    int number = 25;
            System.out.println("Переменная: "+number);
    можно использовать 
    String name = "25";
            System.out.println("Переменная: "+name);

---
+++ task_1.4/main.java
@@ -0,0 +1,14 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+public class Solution {
+    public static int number = 25;
+
+  
+    public static void main(String[] args) {
+        int number = 25;
+        System.out.println("Переменная: "+number);
+        int num1 = 25;
+        System.out.println("Квадрат переменной = "+num1*num1);
+    }
+}
