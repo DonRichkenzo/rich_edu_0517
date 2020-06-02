26186314c4124f532f1451816696c4757ea8a882
DonRichkenzo <66156722+DonRichkenzo@users.noreply.github.com>
2020-06-02 16:39:45 +0300

    Update Main.java

--- Task_2.1/Main.java
+++ Task_2.1/Main.java
@@ -8,14 +8,15 @@
 */
 
 public class Main {
-   public static int min(int a, int b, int c) {
-       if (a < b)
-           return a;
-       if (b < c)
-           return b;
-       else
-           return c;
+    public static int min(int a, int b, int c) {
+        if (a <= b && a <= c)
+            return a;
+        if (b <= a && b <= c)
+            return b;
+        else
+            return c;
     }
+
 
     public static void main(String[] args) {
         System.out.println(min(1, 2, 3));
