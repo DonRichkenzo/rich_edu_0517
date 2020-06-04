013e5c8fb9b3e8ed6bb0d2887102a9b7528ef506
DonRichkenzo <66156722+DonRichkenzo@users.noreply.github.com>
2020-06-04 10:45:12 +0300

    Update main.java

--- Task_2.3/main.java
+++ Task_2.3/main.java
@@ -12,7 +12,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10;
-        System.out.println(a*b*c*d*e*f*g*h*i*j);
+        int a = 1;
+        for(int i = 2; i <= 10; i++)
+            a = a * i;
+        System.out.println(a);
     }
 }
