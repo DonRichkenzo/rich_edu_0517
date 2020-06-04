ee010e53e95ef541e497de2aa68baa0fb57de15a
DonRichkenzo <66156722+DonRichkenzo@users.noreply.github.com>
2020-06-04 11:06:49 +0300

    Update main.java

--- Task_2.4/main.java
+++ Task_2.4/main.java
@@ -18,10 +18,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(1 + 2);
-        System.out.println(1 + 2 +3);
-        System.out.println(1 + 2 + 3 + 4);
-        System.out.println(1 + 2 + 3 + 4 + 5);
+        int a = 0;
+        for (int b = 1; b < 6; b++) {
+            a = a + b;
+            System.out.println(a);
+        }
     }
 }
+
